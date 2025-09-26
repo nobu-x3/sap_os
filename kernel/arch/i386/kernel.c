3e33db5bee4cc4281f50f7c37c44b30332ab3299
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2025-09-26 10:04:52 +0200

    io C helpers

--- kernel/arch/i386/kernel.c
+++ kernel/arch/i386/kernel.c
@@ -1,14 +1,11 @@
 #include <kernel/tty.h>
 #include <kernel/idt.h>
+#include <kernel/io.h>
 #include <stdio.h>
-
-void test(int one, int zero) {
-    printf("%d", one / zero);
-}
 
 void kernel_main() {
     terminal_initialize();
     printf("hello world!\nnext line\n\n\n\n\n\nnext\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nagain");
     idt_init();
-    test(1, 0);
+    outb(0x60, 0xff);
 }
