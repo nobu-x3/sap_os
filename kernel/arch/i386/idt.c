a520b1c407e94a34bed2aa55fd6a482bf8e40169
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2025-09-26 06:42:16 +0200

    IDT setup

---
+++ kernel/arch/i386/idt.c
@@ -0,0 +1,30 @@
+#include <kernel/idt.h>
+#include <stdio.h>
+#include <string.h>
+#include "config.h"
+
+struct idt_desc idt_descriptors[SAP_OS_TOTAL_INTERRUPTS];
+struct idtr_desc idtr_descriptor;
+
+extern void idt_load(struct idtr_desc* ptr);
+
+void idt_div_zero() {
+    printf("Divide by zero error!\n");
+}
+
+void idt_set(int interrupt_number, void* address) {
+    struct idt_desc* desc = &idt_descriptors[interrupt_number];
+    desc->offset_1 = (uint32_t)address & 0x0000ffff;
+    desc->selector = KERNEL_CODE_SELECTOR;
+    desc->zero = 0x00;
+    desc->type_attr = 0xEE;
+    desc->offset_2  = (uint32_t) address >> 16;
+}
+
+void idt_init() {
+    memset(idt_descriptors, 0, sizeof(idt_descriptors));
+    idtr_descriptor.limit = sizeof(idt_descriptors) - 1;
+    idtr_descriptor.base = (uint32_t)idt_descriptors;
+    idt_set(0, idt_div_zero);
+    idt_load(&idtr_descriptor);
+}
--- kernel/arch/i386/kernel.c
+++ kernel/arch/i386/kernel.c
@@ -1,7 +1,14 @@
 #include <kernel/tty.h>
+#include <kernel/idt.h>
 #include <stdio.h>
+
+void test(int one, int zero) {
+    printf("%d", one / zero);
+}
 
 void kernel_main() {
     terminal_initialize();
     printf("hello world!\nnext line\n\n\n\n\n\nnext\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nagain");
+    idt_init();
+    test(1, 0);
 }
